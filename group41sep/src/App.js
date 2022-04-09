7ada3108748bb5175c6cd110f429f92760fac684
vish-hub10 <ec20380@qmul.ac.uk>
2022-04-09 17:46:43 +0100

    first commit

---
+++ group41sep/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <div>
+        <form class="box" action="login.html" method="post"></form>
+      <h1>login page</h1>
+      <input type="text" name="" placeholder="enter username" clasname="username"></input>
+      <input type="password" name="" placeholder="enter password" id="password"></input>
+      <input type="submit" name="" value="login"></input>
+      </div>
+      </body>
+  );
+   
+}
+
+export default App;
